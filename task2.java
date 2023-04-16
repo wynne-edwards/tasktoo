ee5d074422657e7cbe8a06bf8f0ab70642309252
Wynne Edwards <wynnee02@gmail.com>
2023-04-16 16:45:53 +0200

    Added data validation to user input

--- task2.java
+++ task2.java
@@ -7,7 +7,8 @@ public class task2 {
 
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("data.xml"))) {
-            List<String> selectedFields = getSelectedFields();
+            List<String> availableFields = getAvailableFields();
+            List<String> selectedFields = getSelectedFields(availableFields);
             String line;
             List<Map<String, String>> dataList = new ArrayList<>();
             while ((line = br.readLine()) != null) {
@@ -29,20 +30,41 @@ public class task2 {
         }
     }
 
-    private static List<String> getSelectedFields() {
+    private static List<String> getAvailableFields() {
+        List<String> availableFields = new ArrayList<>();
+        availableFields.add("name");
+        availableFields.add("postalZip");
+        availableFields.add("region");
+        availableFields.add("country");
+        availableFields.add("address");
+        availableFields.add("list");
+        return availableFields;
+    }
+
+    private static List<String> getSelectedFields(List<String> availableFields) {
         Scanner scanner = new Scanner(System.in);
-        List<String> fields = new ArrayList<>();
+        List<String> selectedFields = new ArrayList<>();
         while (true) {
             System.out.println("Enter a field name (or 'done' to finish):");
             String field = scanner.nextLine();
             if (field.equals("done")) {
                 break;
+            } else if (!isValidField(field)) {
+                System.out.println("Invalid field name. Please enter a valid field name.");
+            } else if (selectedFields.contains(field)) {
+                System.out.println("Field already selected. Please enter a different field name.");
+            } else if (!availableFields.contains(field)) {
+                System.out.println("Field not available. Please enter a different field name.");
             } else {
-                fields.add(field);
+                selectedFields.add(field);
             }
         }
         scanner.close();
-        return fields;
+        return selectedFields;
+    }
+
+    private static boolean isValidField(String field) {
+        return field.matches("[a-zA-Z]+[a-zA-Z0-9]*");
     }
 
     private static String convertToJSON(List<Map<String, String>> dataList) {
