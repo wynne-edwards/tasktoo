5f24001a2ccc4deb3f1732b18694df63d452dc95
Wynne Edwards <wynnee02@gmail.com>
2023-04-16 16:35:08 +0200

    Prints in JSON format

--- task2.java
+++ task2.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class task2 {
@@ -12,14 +14,17 @@ public class task2 {
         try (BufferedReader br = new BufferedReader(new FileReader("data.xml"))) {
             List<String> selectedFields = getSelectedFields();
             String line;
+            Map<String, String> data = new LinkedHashMap<>();
             while ((line = br.readLine()) != null) {
                 for (String field : selectedFields) {
                     if (line.contains("<" + field + ">")) {
                         String value = line.replaceAll("<" + field + ">|</" + field + ">", "").trim();
-                        System.out.println(field + ": " + value);
+                        data.put(field, value);
                     }
                 }
             }
+            String json = convertToJSON(data);
+            System.out.println(json);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -39,5 +44,16 @@ public class task2 {
         }
         scanner.close();
         return fields;
+    }
+
+    private static String convertToJSON(Map<String, String> data) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{ ");
+        for (String key : data.keySet()) {
+            sb.append("\"" + key + "\" : \"" + data.get(key) + "\", ");
+        }
+        sb.setLength(sb.length() - 2);
+        sb.append(" }");
+        return sb.toString();
     }
 }
