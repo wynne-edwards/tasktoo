3534641ea8ac97a0bc5d50a2f685b7dc899ffe0e
Wynne Edwards <wynnee02@gmail.com>
2023-04-16 16:29:36 +0200

    added ability to only print fields requested by users

--- task2.java
+++ task2.java
@@ -1,36 +1,43 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class task2 {
 
     public static void main(String[] args) {
 
         try (BufferedReader br = new BufferedReader(new FileReader("data.xml"))) {
+            List<String> selectedFields = getSelectedFields();
             String line;
             while ((line = br.readLine()) != null) {
-                if (line.contains("<name>")) {
-                    String name = line.replaceAll("<name>|</name>", "").trim();
-                    System.out.println("Name: " + name);
-                } else if (line.contains("<postalZip>")) {
-                    String postalZip = line.replaceAll("<postalZip>|</postalZip>", "").trim();
-                    System.out.println("Postal Zip: " + postalZip);
-                } else if (line.contains("<region>")) {
-                    String region = line.replaceAll("<region>|</region>", "").trim();
-                    System.out.println("Region: " + region);
-                } else if (line.contains("<country>")) {
-                    String country = line.replaceAll("<country>|</country>", "").trim();
-                    System.out.println("Country: " + country);
-                } else if (line.contains("<address>")) {
-                    String address = line.replaceAll("<address>|</address>", "").trim();
-                    System.out.println("Address: " + address);
-                } else if (line.contains("<list>")) {
-                    String list = line.replaceAll("<list>|</list>", "").trim();
-                    System.out.println("List: " + list);
+                for (String field : selectedFields) {
+                    if (line.contains("<" + field + ">")) {
+                        String value = line.replaceAll("<" + field + ">|</" + field + ">", "").trim();
+                        System.out.println(field + ": " + value);
+                    }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static List<String> getSelectedFields() {
+        Scanner scanner = new Scanner(System.in);
+        List<String> fields = new ArrayList<>();
+        while (true) {
+            System.out.println("Enter a field name (or 'done' to finish):");
+            String field = scanner.nextLine();
+            if (field.equals("done")) {
+                break;
+            } else {
+                fields.add(field);
+            }
+        }
+        scanner.close();
+        return fields;
     }
 }
